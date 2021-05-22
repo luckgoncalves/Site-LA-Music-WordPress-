c1eda0a6bf7a996f0ce1abcd0c7b4e7dd5faaae5
Lucas Goncalves <lucas.goncalves@oystr.com.br>
2021-05-22 20:14:31 -0300

    ajustes filtros blog

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -6,36 +6,61 @@ selectTags = (newTags) => {
     
     search = search.substring(1);
     
-    let tags = 'tag='+ newTags;
-    let cat  = '';
-    let s    = '';
     
+    let newSearchs = ''
     if (search) {
         let searchToObject = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
         
-        let exist = false; 
-        if (searchToObject.tag) {
-            exist = searchToObject.tag.includes(newTags)
-        }
+        Object.keys(searchToObject).map(keys => {
+            newSearchs = {...newSearchs, [keys.replace('?', '')]: searchToObject[keys]}
+        })
+        newSearchs = {...newSearchs, tag: filterTag({tag:searchToObject.tag, newTags})}
 
-        let searchTag = searchToObject.tag ? `${searchToObject.tag},` : '';
-        
-        tags += ','+searchTag;
-        if(exist) {
-            tags = tags.replaceAll(`${newTags},`, '')
-        }
-    
-        let searchCat = searchToObject.cat ? `${searchToObject.cat}&` : '';
-        cat += searchCat ? 'cat='+ searchCat : '';
-        
-        s = searchToObject.s ? `&s=${searchToObject.s}` : '';
+    } else {
+        newSearchs = {...newSearchs, tag: newTags}
     }
-    activeTag();
 
-    url += cat+tags+s;
+    activeTag()
+
+    // CONVERTER OBJETOS DOS FILTROS EM ARRAY;
+    let arrSearchs = []
+    Object.keys(newSearchs).map(keys => 
+        newSearchs[keys].length > 0 && arrSearchs.push(`${keys.replace('?', '')}=${newSearchs[keys]}`)
+    )
     
+    // CONCATENA OS ARRAYS COM '&' para filtro do WP;
+    arrSearchs = arrSearchs.join('&')    
+
     // Rediect 
-    window.location.href = url
+    window.location.href = url + arrSearchs
+}
+
+function filterTag({tag,newTags}) {
+    if(!tag) {
+        return newTags
+    }
+    
+    let arrTags = tag.split(',');
+    
+    if (tag.includes(newTags)) {
+        if (arrTags.length > 0) {
+            arrTags = arrTags.filter(tags => tags !== newTags)
+        }
+    } else {
+        console.log(arrTags)
+        arrTags.push(newTags)
+    }
+    
+    arrTags = arrTags.join(',')
+    
+    return arrTags.length > 0 ? arrTags: ''
+}
+
+function filterCat ({cat}) {
+    if(!cat) {
+        return ''
+    }
+    return `cat=${cat}`
 }
 
 function activeTag () {
@@ -43,6 +68,7 @@ function activeTag () {
     
     if (search) {
         let searchToObject = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+        console.log(searchToObject)
         
         if (searchToObject.tag) {
             let arrTags = searchToObject.tag.split(',')
@@ -63,27 +89,34 @@ function getSearch() {
     let url = location.origin +location.pathname+'?';
 
     let value = $('#search-post').val()
+
+    // ZERA O TEMPO EM QUE EXECUTA A FUNCAO PARA FAZER O FILTRO DO CAMPO DE #SEARCH-POST;
     clearInterval(interval)
     interval = setTimeout(() => {
         
-        // console.log(`${location.href}&s=${value}`)
+        let newSearchs = ''
         if(location.search) {
             let searchToObject = JSON.parse('{"' + decodeURI(location.search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-            console.log(searchToObject)
-            let newSearchs = ''
-            Object.keys(searchToObject).forEach(keys => {
-                let newkeys = keys.replace('?','')
-
-                if (keys === 's'){
-                    newSearchs += `&${newkeys}=${value}`;
-                } else {
-                    newSearchs += `&${newkeys}=${searchToObject[keys]}`
-                }
-            })
             
-            window.location.href = `${url}?${newSearchs}`;
+            Object.keys(searchToObject).map(keys => {
+                newSearchs = {...newSearchs, [keys.replace('?', '')]: searchToObject[keys]}
+            })
+            newSearchs = {...newSearchs, s: value}
+
+        } else {
+            newSearchs = {...newSearchs, s: value}
         }
+
+        // CONVERTER OBJETOS DOS FILTROS EM ARRAY;
+        let arrSearchs = []
+        Object.keys(newSearchs).map(keys => 
+            newSearchs[keys].length > 0  && arrSearchs.push(`${keys.replace('?', '')}=${newSearchs[keys]}`)
+        )
+
+        // CONCATENA OS ARRAYS COM '&' para filtro do WP;
+        arrSearchs = arrSearchs.join('&')
+        
+        window.location.href = `${url}${arrSearchs}`;
     
-        window.location.href = `?s=${value}`;
-    }, 550);
+    }, 1000);
 }
